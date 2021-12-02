4ce2fafe3284585f1c567c1f653ad61a9ff815bd
= <54820491+mfrkvrl@users.noreply.github.com>
2021-12-02 19:15:45 +0300

    Äilk commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double boy;
+        double kilo;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinden) giriniz : ");
+        boy=scanner.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz : ");
+        kilo=scanner.nextDouble();
+        double index=(kilo/(boy*boy));
+        System.out.println("Vücut kütle indexiniz : " + index);
+    }
+}
